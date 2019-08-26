602b8f1c0851a88ae44dc81f562d56fc5b812cee
yuchanglong <yuchanglong@epipechina.com>
2019-08-26 10:44:13 +0800

    Initial application generated by JHipster-5.2.1

---
+++ src/main/java/com/yu/mybatisplus/config/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package com.yu.mybatisplus.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.yu.mybatisplus")
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+}
---
+++ src/main/java/com/yu/mybatisplus/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.yu.mybatisplus.config.audit;
